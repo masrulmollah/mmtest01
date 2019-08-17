ba01c8ebf808f63a72a497f50c930469c2f6fe30
masrulmollah <54163010+masrulmollah@users.noreply.github.com>
2019-08-17 10:32:44 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.write("Md. Masrul Mollah");
 
-document.write("I am learning website development with javascript")
+document.write("I am learning website development with javascript");
 
 document.write("Muhammad Ibne Masrul is my son");
 
+![]<>
